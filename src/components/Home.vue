862c41ce6be2b8041c04c19a562cbce09fa7b6e9
VH Phan <phanveehuen@gmail.com>
2022-03-26 22:41:29 +0800

    Finished live chart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,11 +5,11 @@
       style="height: 100vh;"
   >
 
-    <template v-slot:before>
-      <div class="q-pa-md">
-        <div class="text-h4 q-mb-md">Before</div>
-
-
+    <template v-slot:before >
+      <q-resize-observer @resize="onResize"/>
+      <div class="q-pa-md" style="max-height: 40vh;" ref="chartContainer">
+        <div class="text-h4 q-mb-md" ref="chartRef">
+        </div>
       </div>
     </template>
 
@@ -23,19 +23,19 @@
           <template v-slot:before>
 
             <q-table
-            :rows="quotes"
-            :pagination="{rowsPerPage: 8}"
-            dense
-            dark
+                :rows="quotes"
+                :pagination="{rowsPerPage: 8}"
+                dense
+                dark
             ></q-table>
 
           </template>
           <template v-slot:after>
             <q-table
-            :rows="trades"
-            :pagination="{rowsPerPage: 8}"
-            dense
-            dark
+                :rows="trades"
+                :pagination="{rowsPerPage: 8}"
+                dense
+                dark
             ></q-table>
 
           </template>
@@ -49,7 +49,9 @@
 
 
 <script>
-import {computed, ref} from 'vue'
+import {computed, onMounted, ref, watch} from 'vue'
+import {createChart, CrosshairMode} from "lightweight-charts";
+import {useCandleChart} from "../composables/candleSticks";
 
 export default {
   name: 'Home',
@@ -68,46 +70,106 @@ export default {
     const bars = ref([]);
     const latestBar = computed(() => bars.value[bars.value.length - 1]);
 
-    const processData = (d) => {
-      if (!d.T) return;
-      const dataType = d.T;
+    const chartRef = ref(null);
+    const chartContainer = ref(null);
+    const start = new Date(Date.now() - 7200 * 1000).toISOString();
+    const myCandleChart = useCandleChart();
+
+    let chart, candleSeries, currentBar;
+
+    onMounted(async () => {
+
+      const obj = await myCandleChart(chartRef.value, start);
+      chart = obj.chart;
+      candleSeries = obj.candleSeries;
+      currentBar = obj.currentBar;
+
+
+      socket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+        const message = data[0]["msg"];
+
+        data.forEach(d => {
+          processData(d);
+        })
+
+        if (message === "connected") {
+          console.log("do authentication");
+          socket.send(JSON.stringify(auth));
+        }
+
+        if (message === "authenticated") {
+          socket.send(JSON.stringify(subscribe));
+        }
+      }
+
+      if (chart) {
+        chart.resize(chartContainer.value.offsetWidth, chartContainer.value.offsetHeight - 40);
+      }
+
+      // setTimeout(() => {
+      //   console.log(chartContainer.value.offsetWidth);
+      //   chart.resize(chartContainer.value.offsetWidth, chartContainer.value.offsetHeight - 40);
+      // }, 5000);
+
+    })
+
+
+
+    const processData = (bar) => {
+      if (!bar.T) return;
+      const dataType = bar.T;
       switch (dataType) {
         case 'q':
-          const q = {time: d.t, bid: d.bp, ask: d.ap};
+          const q = {time: bar.t, bid: bar.bp, ask: bar.ap};
           quotes.value.push(q);
           if (quotes.value.length > 8) quotes.value.shift();
           break;
         case 't':
-          const t = {time: d.t, price: d.p, size: d.s};
-          trades.value.push(t);
+          const t = {time: bar.t, price: bar.p, size: bar.s};
+          trades.value.push(t.price);
           if (trades.value.length > 8) trades.value.shift();
+
+          let open = trades.value[0];
+          let high = Math.max(...trades.value);
+          let low = Math.min(...trades.value);
+          let close = trades.value[trades.value.length - 1];
+
+          candleSeries.update({
+            time: currentBar.time + 60,
+            open: open,
+            high: high,
+            low: low,
+            close: close,
+          });
           break;
+
         case 'b':
-          bars.value.push(d);
+          bars.value.push(bar);
           if (bars.value.length > 1000) bars.value.shift();
+
+          const timestamp = new Date(bar.t).getTime() / 1000;
+
+          currentBar = {
+            time: timestamp,
+            open: bar.o,
+            high: bar.h,
+            low: bar.l,
+            close: bar.c,
+          };
+
+          candleSeries.update(currentBar);
+          trades.value = [];
+
           break;
       }
     }
 
-
-    socket.onmessage = function (event) {
-      const data = JSON.parse(event.data);
-      const message = data[0]["msg"];
-
-      data.forEach(d => {
-        processData(d);
-      })
-
-      if (message === "connected") {
-        console.log("do authentication");
-        socket.send(JSON.stringify(auth));
-      }
-
-      if (message === "authenticated") {
-        socket.send(JSON.stringify(subscribe));
-      }
+    const onResize = (size) => {
+      console.log(size);
+      if (!chart) return;
+      chart.resize(size.width, size.height - 40);
     }
-
 
     return {
       splitterModel: ref(50),
@@ -116,6 +178,9 @@ export default {
       trades,
       bars,
       latestBar,
+      chartRef,
+      onResize,
+      chartContainer
 
     }
   }
---
+++ src/composables/candleSticks.js
@@ -0,0 +1,60 @@
+import {createChart, CrosshairMode} from "lightweight-charts";
+import {ref} from "vue";
+
+export const useCandleChart = () => {
+    return async function (chartRef, start) {
+        const chart = createChart(chartRef, {
+            width: 700,
+            height: 250,
+            layout: {
+                backgroundColor: "#000000",
+                textColor: "#ffffff",
+            },
+            grid: {
+                vertLines: {
+                    color: "#404040",
+                },
+                horzLines: {
+                    color: "#404040",
+                },
+            },
+            crosshair: {
+                mode: CrosshairMode.Normal,
+            },
+            priceScale: {
+                borderColor: "#cccccc",
+            },
+            timeScale: {
+                borderColor: "#cccccc",
+                timeVisible: true,
+            },
+        });
+        const candleSeries = chart.addCandlestickSeries();
+        const barsUrl =
+            "https://data.alpaca.markets/v1beta1/crypto/ETHUSD/bars?exchanges=CBSE&timeframe=1Min&start=" +
+            start;
+        const apiKey = import.meta.env.VITE_API_KEY
+        const apiSecret = import.meta.env.VITE_API_SECRET
+        let currentBar = {};
+        const r = await fetch(barsUrl, {
+            headers: {
+                "APCA-API-KEY-ID": apiKey,
+                "APCA-API-SECRET-KEY": apiSecret,
+            }
+        })
+        const response = await r.json();
+        const data = response.bars.map((bar) => ({
+            open: bar.o,
+            high: bar.h,
+            low: bar.l,
+            close: bar.c,
+            time: Date.parse(bar.t) / 1000,
+        }));
+        currentBar = data[data.length - 1];
+        console.log(data);
+        candleSeries.setData(data);
+        return {chart, candleSeries, currentBar};
+    }
+
+
+};
